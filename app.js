dd6ec46de348986816d8089cebeb8cb43e5d6aeb
DESKTOP-CU30T8T\UHD CARREFOUR <imen.hnayet@gmail.com>
2023-01-22 22:03:30 +0200

    add comment sourcetree

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+/*** test source tree **/
 app.use('/api', indexRouter);
 
 module.exports = app;
