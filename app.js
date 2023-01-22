b994300c06e8cd7fe929c0d5eebd444a73a81e86
imen20 <imen.hnayet@gmail.com>
2023-01-22 21:16:03 +0200

    comment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
